929740c2ee95db3ecb5aed0cf31e331748792dfb
ZayuR3X <108033271+ZayuR3X@users.noreply.github.com>
2022-12-26 17:47:09 +0300

    Update soru-6.cpp

--- questions/2020-2021-Final/soru-6.cpp
+++ questions/2020-2021-Final/soru-6.cpp
@@ -8,7 +8,7 @@ int main() {
     srand (time(NULL)); // bu olmazsa hep aynı sayıları verir
     
     for (int i = 0; i < 8; i++) {
-        cout << rand() % 100 << " ";
+        cout << (rand() % 100) + 1 << " "; // +1 eklememin sebebi 100'ü dahil etmek için
         }    
        
     return 0;
