a1899cae9a9e50688f905922f872cbd652d0ce8f
ZayuR3X <108033271+ZayuR3X@users.noreply.github.com>
2022-12-30 11:00:44 +0300

    Create namespace.cpp

---
+++ src/namespace.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+namespace uzay
+{
+	int a = 10, b = 20;
+}
+using namespace uzay;
+
+
+int main () {
+		
+	//std::cout << uzay::a;
+	cout << a;
+	return 0;
+}
