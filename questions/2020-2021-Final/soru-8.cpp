e8edf13f8b34039d735ac53d0a018abd624d460b
ZayuR3X <108033271+ZayuR3X@users.noreply.github.com>
2023-01-04 13:03:05 +0300

    Create soru-8.cpp

---
+++ questions/2020-2021-Final/soru-8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int y,x = 5,a = 2,b = 3 ,c = 7;
+    
+    y = a*x*x + b*x + c;
+    
+    cout << "Sonuc: " << y << endl;
+    
+    return 0;
+}
